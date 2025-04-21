deb054bd9e2cda58c2bc65f97c6319273b549c68
yeisonback9 <yeison.rojas@back9.com.ve>
2025-04-20 20:10:24 -0400

    add .env

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import corsOptions from "./middlewares/cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = "3000";
+const HOST_IP = process.env.HOST_IP || "127.0.0.1";
+const PORT = process.env.HTTP_PORT || "3000";
 const corsMiddleware = cors(corsOptions);
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +19,6 @@ app.get("/", (req, res) => {
   res.json({ project: "TEST-AWS-API", author: "Yeison Rojas" });
 });
 
-app.listen(port, () => {
-  console.log(`Listening in http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening in http://${HOST_IP}:${PORT}`);
 });
